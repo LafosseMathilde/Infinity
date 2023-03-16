d402fbd2e61e6462f907e4f50acf580b01a32b14
LafosseMathilde <98532717+LafosseMathilde@users.noreply.github.com>
2023-03-16 16:17:15 +0100

    Update test3.php

--- test3.php
+++ test3.php
@@ -1,61 +1,50 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Calendrier dynamique avec PHP et HTML</title>
-</head>
-<body>
-    <h1>Calendrier dynamique avec PHP et HTML</h1>
+<?php
+// Définition du fuseau horaire
+date_default_timezone_set('Europe/Paris');
 
-    <?php
-        // Récupération des données des utilisateurs depuis une base de données ou autre source
-        // Exemple :
-        $users = array(
-            array(
-                "name" => "John Doe",
-                "email" => "john.doe@example.com"
-            ),
-            array(
-                "name" => "Jane Smith",
-                "email" => "jane.smith@example.com"
-            )
-        );
+// Récupération de la date actuelle
+$date = new DateTime();
+$year = $date->format('Y');
+$month = $date->format('n');
 
-        // Récupération de la date actuelle
-        $today = date("Y-m-d");
+// Nombre de jours dans le mois en cours
+$numDays = cal_days_in_month(CAL_GREGORIAN, $month, $year);
 
-        // Affichage du calendrier
-        echo "<table>";
-        echo "<thead>";
-        echo "<tr>";
-        echo "<th></th>";
-        for ($i = 0; $i < 7; $i++) {
-            $day = date("Y-m-d", strtotime($today . " +$i day"));
-            echo "<th colspan='2'>$day</th>";
+// Jour de la semaine du premier jour du mois
+$firstDay = date('N', strtotime($year . '-' . $month . '-01'));
+
+// Création du tableau HTML
+echo '<table>';
+echo '<tr><th>Lun</th><th>Mar</th><th>Mer</th><th>Jeu</th><th>Ven</th><th>Sam</th><th>Dim</th></tr>';
+
+// Boucle pour chaque semaine
+for ($week = 1; $week <= ceil(($numDays + $firstDay - 1) / 7); $week++) {
+    echo '<tr>';
+
+    // Boucle pour chaque jour de la semaine
+    for ($day = 1; $day <= 7; $day++) {
+        // Calcul du numéro de jour dans le mois
+        $numDay = ($week - 1) * 7 + $day - $firstDay + 1;
+
+        // Si le numéro de jour est compris entre 1 et le nombre de jours dans le mois, on affiche le jour
+        if ($numDay >= 1 && $numDay <= $numDays) {
+            echo '<td>';
+            echo '<select name="data[' . $year . '-' . $month . '-' . $numDay . ']">';
+            echo '<option value=""></option>';
+            echo '<option value="option1">Option 1</option>';
+            echo '<option value="option2">Option 2</option>';
+            echo '<option value="option3">Option 3</option>';
+            echo '</select>';
+            echo '<br>';
+            echo $numDay;
+            echo '</td>';
+        } else {
+            echo '<td></td>';
         }
-        echo "</tr>";
-        echo "</thead>";
-        echo "<tbody>";
-        foreach ($users as $user) {
-            echo "<tr>";
-            echo "<td>{$user['name']}</td>";
-            for ($i = 0; $i < 7; $i++) {
-                $day = date("Y-m-d", strtotime($today . " +$i day"));
-                $data_am = ""; // Données à afficher pour la demi-journée du matin pour ce jour
-                $data_pm = ""; // Données à afficher pour la demi-journée de l'après-midi pour ce jour
-                // Récupération des données de l'utilisateur pour ce jour depuis une base de données ou autre source
-                // Exemple :
-                // $data_am = ...;
-                // $data_pm = ...;
-                echo "<td><input type='text' name='data_am[$user[name]][$day]' value='$data_am'></td>";
-                echo "<td><input type='text' name='data_pm[$user[name]][$day]' value='$data_pm'></td>";
-            }
-            echo "</tr>";
-        }
-        echo "</tbody>";
-        echo "</table>";
-    ?>
+    }
 
-    <input type="submit" value="Enregistrer les données">
-</body>
-</html>
+    echo '</tr>';
+}
+
+echo '</table>';
+?>
