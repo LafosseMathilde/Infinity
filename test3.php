bae0a8ad489d4287910f5cf7ee8725aaec7e58e1
KarneevThomas <98533355+KarneevThomas@users.noreply.github.com>
2023-03-17 11:00:08 +0100

    Update test3.php

--- test3.php
+++ test3.php
@@ -2,8 +2,13 @@
 // Définition du fuseau horaire
 date_default_timezone_set('Europe/Paris');
 
-// Récupération de la date actuelle
-$date = new DateTime();
+// Récupération de la date actuelle ou de la date passée en paramètre "date"
+if (isset($_GET['date'])) {
+    $date = new DateTime($_GET['date']);
+} else {
+    $date = new DateTime();
+}
+
 $year = $date->format('Y');
 $month = $date->format('n');
 
@@ -13,8 +18,15 @@ $numDays = cal_days_in_month(CAL_GREGORIAN, $month, $year);
 // Jour de la semaine du premier jour du mois
 $firstDay = date('N', strtotime($year . '-' . $month . '-01'));
 
-// Affichage du mois et de l'année
-echo '<h1>' . date('F Y', strtotime($year . '-' . $month . '-01')) . '</h1>';
+// Affichage du mois et de l'année avec boutons pour passer au mois précédent et suivant
+echo '<h1>';
+echo '<a href="?date=' . $date->modify('-1 month')->format('Y-m-d') . '">&lt;</a> ';
+echo date('F Y', strtotime($year . '-' . $month . '-01'));
+echo ' <a href="?date=' . $date->modify('+2 months')->format('Y-m-d') . '">&gt;</a>';
+echo '</h1>';
+
+// Réinitialisation de la date
+$date->modify('-1 month');
 
 // Création du tableau HTML
 echo '<table>';
@@ -32,35 +44,50 @@ for ($week = 1; $week <= ceil(($numDays + $firstDay - 1) / 7); $week++) {
         // Si le numéro de jour est compris entre 1 et le nombre de jours dans le mois, on affiche le jour
         if ($numDay >= 1 && $numDay <= $numDays) {
             echo '<td>';
-            echo '<select name="data[' . $year . '-' . $month . '-' . $numDay . '_matin]">';
-            echo '<option value="AM">AM</option>';
-            echo '<option value="Conges">Conges</option>';
-            echo '<option value="Maladie">Maladie</option>';
-            echo '<option value="Teletravail">Teletravail</option>';
-            echo '<option value="Formation">Formation</option>';
-            echo '<option value="RTT">RTT</option>';
-            echo '<option value="SurSite">Sur Site</option>';
-            echo '</select>';
-            echo '<br>';
-            echo '<select name="data[' . $year . '-' . $month . '-' . $numDay . '_apresmidi]">';
-            echo '<option value="PM">PM</option>';
-            echo '<option value="Conges">Conges</option>';
-            echo '<option value="Maladie">Maladie</option>';
-            echo '<option value="Teletravail">Teletravail</option>';
-            echo '<option value="Formation">Formation</option>';
-            echo '<option value="RTT">RTT</option>';
-            echo '<option value="SurSite">Sur Site</option>';
-            echo '</select>';
-            echo '<br>';
-            echo $numDay;
-            echo '</td>';
-        } else {
-            echo '<td></td>';
-        }
-    }
+echo '<select name="data[' . $year . '-' . $month . '-' . $numDay . '_matin]">';
+echo '<option value="AM">AM</option>';
+echo '<option value="Conges">Conges</option>';
+echo '<option value="Maladie">Maladie</option>';
+echo '<option value="Teletravail">Teletravail</option>';
+echo '<option value="Formation">Formation</option>';
+echo '<option value="RTT">RTT</option>';
+echo '<option value="SurSite">Sur Site</option>';
+echo '</select>';
+echo '<br>';
+echo '<select name="data[' . $year . '-' . $month . '-' . $numDay . '_apresmidi]">';
+echo '<option value="PM">PM</option>';
+echo '<option value="Conges">Conges</option>';
+echo '<option value="Maladie">Maladie</option>';
+echo '<option value="Teletravail">Teletravail</option>';
+echo '<option value="Formation">Formation</option>';
+echo '<option value="RTT">RTT</option>';
+echo '<option value="SurSite">Sur Site</option>';
+echo '</select>';
+echo '<br>';
+echo $numDay;
+echo '</td>';
+} else {
+echo '<td></td>';
+}
 
-    echo '</tr>';
+// Fin de la boucle pour chaque jour de la semaine
+}
+
+echo '</tr>';
 }
 
 echo '</table>';
+
+// Boutons pour afficher le mois suivant ou précédent
+echo '<form method="get">';
+echo '<input type="hidden" name="year" value="' . $year . '">';
+echo '<input type="hidden" name="month" value="' . ($month - 1) . '">';
+echo '<input type="submit" value="Mois précédent">';
+echo '</form>';
+
+echo '<form method="get">';
+echo '<input type="hidden" name="year" value="' . $year . '">';
+echo '<input type="hidden" name="month" value="' . ($month + 1) . '">';
+echo '<input type="submit" value="Mois suivant">';
+echo '</form>';
 ?>
