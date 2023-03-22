d3f12cb5ac68ea336c9c939541bc632049cdb106
LafosseMathilde <98532717+LafosseMathilde@users.noreply.github.com>
2023-03-22 15:04:25 +0100

    Update testmathilde.php

--- testmathilde.php
+++ testmathilde.php
@@ -1,72 +1,87 @@
 <?php
-// Tableau contenant les jours de la semaine
-$jours_semaine = array('Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche');
+// Définition du fuseau horaire
+date_default_timezone_set('Europe/Paris');
 
-// Tableau contenant les demi-journées
-$demi_journees = array('Matin', 'Après-midi');
-
-// Tableau multidimensionnel contenant les données du calendrier
-$calendrier = array(
-    array('Lundi', array('', '')),
-    array('Mardi', array('', '')),
-    array('Mercredi', array('', '')),
-    array('Jeudi', array('', '')),
-    array('Vendredi', array('', '')),
-    array('Samedi', array('', '')),
-    array('Dimanche', array('', ''))
-);
-
-// Traitement du formulaire
-if (isset($_POST['enregistrer'])) {
-    // Récupération des données du formulaire
-    $jour = $_POST['jour'];
-    $demi_journee = $_POST['demi_journee'];
-    $donnees = $_POST['donnees'];
-    // Mise à jour du tableau multidimensionnel
-    $calendrier[$jour][1][$demi_journee] = $donnees;
+// Récupération de la date actuelle ou de la date passée en paramètre "date"
+if (isset($_GET['date'])) {
+    $date = new DateTime($_GET['date']);
+} else {
+    $date = new DateTime();
 }
 
-?>
+$year = $date->format('Y');
+$month = $date->format('n');
 
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Calendrier dynamique</title>
-</head>
-<body>
-    <h1>Calendrier dynamique</h1>
-    <form method="post">
-        <table border="1">
-            <tr>
-                <th>Jour</th>
-                <th>Matin</th>
-                <th>Après-midi</th>
-            </tr>
-            <?php foreach ($calendrier as $index => $ligne): ?>
-                <tr>
-                    <td><?php echo $ligne[0]; ?></td>
-                    <?php foreach ($demi_journees as $demi_journee): ?>
-                        <td>
-                            <select name="donnees">
-                                <option value="Teletravail">Teletravail</option>
-                                <option value="Conges">Conges</option>
-                                <option value="Maladie">Maladie</option>
-                                <option value="Formation">Formation</option>
-                                <option value="RTT">RTT</option>
-                                <option value="SurSite">Sur Site</option>
-                            </select>
-                            <input type="hidden" name="jour" value="<?php echo $index; ?>">
-                            <input type="hidden" name="demi_journee" value="<?php echo $demi_journee; ?>">
-                            <input type="submit" name="enregistrer" value="Enregistrer">
-                            <?php if(isset($calendrier[$index][1][$demi_journee])): ?>
-                                <?php echo $calendrier[$index][1][$demi_journee]; ?>
-                            <?php endif; ?>
-                        </td>
-                    <?php endforeach; ?>
-                </tr>
-            <?php endforeach; ?>
-        </table>
-    </form>
-</body>
-</html>
+// Nombre de jours dans le mois en cours
+$numDays = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+
+// Jour de la semaine du premier jour du mois
+$firstDay = date('N', strtotime($year . '-' . $month . '-01'));
+
+// Affichage du mois et de l'année avec boutons pour passer au mois précédent et suivant
+echo '<h1>';
+echo '<a href="?date=' . $date->modify('-1 month')->format('Y-m-d') . '">&lt;</a> ';
+echo date('F Y', strtotime($year . '-' . $month . '-01'));
+echo ' <a href="?date=' . $date->modify('+2 months')->format('Y-m-d') . '">&gt;</a>';
+echo '</h1>';
+
+// Réinitialisation de la date
+$date->modify('-1 month');
+
+// Création du tableau HTML
+echo '<table>';
+echo '<tr><th>Lun</th><th>Mar</th><th>Mer</th><th>Jeu</th><th>Ven</th><th>Sam</th><th>Dim</th></tr>';
+
+// Boucle pour chaque semaine
+for ($week = 1; $week <= ceil(($numDays + $firstDay - 1) / 7); $week++) {
+    echo '<tr>';
+
+    // Boucle pour chaque jour de la semaine
+    for ($day = 1; $day <= 7; $day++) {
+        // Calcul du numéro de jour dans le mois
+        $numDay = ($week - 1) * 7 + $day - $firstDay + 1;
+
+        // Si le numéro de jour est compris entre 1 et le nombre de jours dans le mois, on affiche le jour
+        if ($numDay >= 1 && $numDay <= $numDays) {
+            echo '<td>';
+echo '<select name="data[' . $year . '-' . $month . '-' . $numDay . '_matin]">';
+echo '<option value="" disabled selected>AM</option>';
+echo '<option value="Conges">Conges</option>';
+echo '<option value="Maladie">Maladie</option>';
+echo '<option value="Teletravail">Teletravail</option>';
+echo '<option value="Formation">Formation</option>';
+echo '<option value="RTT">RTT</option>';
+echo '<option value="SurSite">Sur Site</option>';
+echo '</select>';
+echo '<br>';
+echo '<select name="data[' . $year . '-' . $month . '-' . $numDay . '_apresmidi]">';
+echo '<option value="" disabled selected>PM</option>';
+echo '<option value="Conges">Conges</option>';
+echo '<option value="Maladie">Maladie</option>';
+echo '<option value="Teletravail">Teletravail</option>';
+echo '<option value="Formation">Formation</option>';
+echo '<option value="RTT">RTT</option>';
+echo '<option value="SurSite">Sur Site</option>';
+echo '</select>';
+echo '<br>';
+echo $numDay;
+echo '</td>';
+} else {
+echo '<td></td>';
+}
+// Si on est arrivé au dernier jour de la semaine, on ferme la ligne du tableau
+if ($day == 7) {
+echo '</tr>';
+}
+// Fin de la boucle pour chaque jour de la semaine
+}
+// Fin de la boucle pour chaque semaine
+}
+
+echo '</table>';
+
+// Ajout d'un bouton de soumission pour envoyer uniquement les cases remplies
+echo '<form method="POST" action="traitement.php">';
+echo '<input type="submit" value="Envoyer">';
+echo '</form>';
+?>
